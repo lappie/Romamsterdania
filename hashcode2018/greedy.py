b331ee9ba43adfac3ea01dbecd1e047077463853
Vlad <vlad.lep@gmail.com>
2018-03-01 22:26:01 +0100

    try to get rides closest to you

--- hashcode2018/greedy.py
+++ hashcode2018/greedy.py
@@ -12,7 +12,7 @@ def sort_compare_function(item1, item2):
 
 def prepare_rides(rides, vehicle, current_time):
     #look at the next
-    rides.sort(key=lambda a_ride: a_ride.distance - vehicle.distance_to_ride(a_ride) if current_time  > a_ride.s
+    rides.sort(key=lambda a_ride: a_ride.distance - vehicle.distance_to_ride(a_ride) if current_time > a_ride.s
                     else a_ride.distance - vehicle.distance_to_ride(a_ride) - (a_ride.s - current_time), reverse=True)
     return rides
 
@@ -28,8 +28,20 @@ def greedy(rows, columns, cars, bonus,  T, rides):
                     break
                 # get sub-list of next rides
 
-                prepare_rides(rides, vehicle, t)
+                # last_change_to_catch_rides = [ride for ride in rides if t + vehicle.distance_to_ride() + ride.distance == ride.f]
+                # sub_rides = rides[0: len(rides)/10] # first 10% of rides
+
+                # sub_rides = [a_ride for a_ride in rides if (a_ride.f - t) < 100]
+                # if not len(sub_rides):
+                #     sub_rides = rides
+
+                # try to re-order first rides based on how close they are to me
+                rides.sort(key=lambda a_ride: vehicle.distance_to_ride(a_ride) if t > a_ride.s else vehicle.distance_to_ride(a_ride) + a_ride.s - t)
+
+                # prepare_rides(rides[:100])
                 for i in range(len(rides)):
                     if vehicle.can_finish_ride(rides[i], t):
                         vehicle.add_ride(rides.pop(i))
+                        # vehicle.add_ride(sub_rides[i])
+                        # rides.remove(sub_rides[i])
                         break
