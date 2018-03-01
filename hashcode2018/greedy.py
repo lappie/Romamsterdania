4027a52f9398f67233ca32039eb874f449e2fccc
Vlad <vlad.lep@gmail.com>
2018-03-01 20:41:47 +0100

    fix sorting list of rides

--- hashcode2018/greedy.py
+++ hashcode2018/greedy.py
@@ -2,7 +2,7 @@ from ride import Ride
 from vehicle import Vehicle
 
 
-def sort_compare_funciton(item1, item2):
+def sort_compare_function(item1, item2):
     if item1.s < item2.s:
         return False
     if item1.s == item2.s:
@@ -11,14 +11,13 @@ def sort_compare_funciton(item1, item2):
 
 
 def greedy(rows, columns, cars, bonus,  T, rides):
+    sorted(reversed(rides), cmp=sort_compare_function)
 
-    sorted_rides = sorted(reversed(rides), cmp=sort_compare_funciton)
-    print sorted_rides
     for t in range(T):
         for vehicle in cars:
             vehicle.try_to_end_ride(t)
             if not vehicle.is_in_use:
                 # get a ride
-                if not len(sorted_rides):
+                if not len(rides):
                     break
-                vehicle.add_ride(sorted_rides.pop(0))
+                vehicle.add_ride(rides.pop(0))
