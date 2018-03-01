2571b8882e7de0ccfd3b0023f542f1e798bcaadc
TudorPaun <valhallen13@gmail.com>
2018-03-01 18:26:49 +0100

    separated tudor main

--- hashcode2018/main_tudor.py
+++ hashcode2018/main_tudor.py
@@ -1,5 +1,18 @@
+from read_write_function import read_file, write_file
+
+def doGoogle(input_data):
+    print input_data
+    out = input_data
+    return out
+
+
+
+
 
 def main():
-    out = 'Tudor!!!'
-    print out
-    return out
+    input_data = read_file('./input/input.txt')
+    out = doGoogle(input_data)
+    write_file("testing.out", "", [])
+
+if __name__ == '__main__':
+    main()
